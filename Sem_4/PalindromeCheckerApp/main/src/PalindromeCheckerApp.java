76a24709a7beff88aa50d76d423c2f4a4cea2e6e
AadityaChaturvedy <aadityachaturvedy@gmail.com>
2026-02-25 08:34:02 +0530

    UC1 Added PalindromeCheckerApp welcome Screen

---
+++ Sem_4/PalindromeCheckerApp/main/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+
+        double version = 1.0;
+
+        System.out.printf("Welcome to the Palindrome Checker Management System");
+        System.out.printf("\nVersion : %s", version);
+        System.out.printf("\nSystem initialized successfully");
+
+    }
+}
